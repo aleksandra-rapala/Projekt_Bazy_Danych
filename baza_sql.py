78b80b7f61c54b248d921d3baf86996b144d54a5
Pawel Zapior <56636514+zapiorpawel@users.noreply.github.com>
2023-06-04 00:36:19 +0200

    Czasy do SQL

--- baza_sql.py
+++ baza_sql.py
@@ -1,4 +1,5 @@
 import psycopg2
+import time
 
 def connect_sql():
     # Połączenie z bazą danych PostgreSQL
@@ -12,21 +13,34 @@ def disconnect_sql(cur_pg, conn_pg):
     conn_pg.close()
 
 def sql_select(cur_pg):
-     # Wykonanie polecenia SELECT
+    start_time = time.time()  # Początkowy czas wykonania zapytania
+    # Wykonanie polecenia SELECT
     cur_pg.execute('''SELECT * from game_types''')
     rows_pg = cur_pg.fetchall()
-    #for hostname in rows_pg:
-    #    print(hostname)
-    return rows_pg
+    end_time = time.time()  # Końcowy czas wykonania zapytania
+    execution_time = end_time - start_time  # Czas wykonania zapytania
+    return rows_pg, execution_time
 
 def sql_delete(cur_pg, conn_pg):
+    start_time = time.time()  # Początkowy czas wykonania zapytania
     cur_pg.execute('''DELETE FROM game_types WHERE name_game_type='orange';''');
     conn_pg.commit()
+    end_time = time.time()  # Końcowy czas wykonania zapytania
+    execution_time = end_time - start_time  # Czas wykonania zapytania
+    return execution_time
 
 def sql_update(cur_pg, conn_pg):
+    start_time = time.time()  # Początkowy czas wykonania zapytania
     cur_pg.execute('''UPDATE game_types SET name_game_type = REPLACE(name_game_type, 'blu', 'blue');''');
     conn_pg.commit()
+    end_time = time.time()  # Końcowy czas wykonania zapytania
+    execution_time = end_time - start_time  # Czas wykonania zapytania
+    return execution_time
 
 def sql_insert(cur_pg, conn_pg):
+    start_time = time.time()  # Początkowy czas wykonania zapytania
     cur_pg.execute('''INSERT INTO game_types (id_game_type, name_game_type) VALUES (555, 'orange');''');
     conn_pg.commit()
+    end_time = time.time()  # Końcowy czas wykonania zapytania
+    execution_time = end_time - start_time  # Czas wykonania zapytania
+    return execution_time
